9f486521f3bb5e64d0e723a2bc90d7c36133fb70
anirachalanzgoumbanlaroui <m_dabouz@estin.dz>
2026-07-19 20:54:09 +0000

    fix: add correct base dir and venv path to passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,14 @@
 import os
 import sys
 
-sys.path.insert(0, "/home/apqnnbvs/api.bara3im-shoot.com/bara3im-shoot/")
+BASE_DIR = "/home/apqnnbvs/api.bara3im-shoot.com/bara3im-shoot"
+
+sys.path.insert(0, BASE_DIR)
+
+# Add virtualenv site-packages so Django & dependencies are importable
+VENV_PATH = "/home/apqnnbvs/api.bara3im-shoot.com/bara3im-shoot/venv/lib/python3.10/site-packages"
+if os.path.exists(VENV_PATH):
+    sys.path.insert(0, VENV_PATH)
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
