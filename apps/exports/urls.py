424112ad43d117ae565d46741e000e7800ed08e7
anirachalanzgoumbanlaroui <m_dabouz@estin.dz>
2026-09-12 14:51:26 +0000

    solve excel error problem3

--- apps/exports/urls.py
+++ apps/exports/urls.py
@@ -3,11 +3,19 @@ from apps.exports import views
 
 urlpatterns = [
     path('center/', views.ExportCenterOverviewView.as_view(), name='export_center_overview'),
+    path('center', views.ExportCenterOverviewView.as_view()),
     path('employee/<str:employee_id>/excel/', views.EmployeeExcelExportView.as_view(), name='export_employee_excel'),
+    path('employee/<str:employee_id>/excel', views.EmployeeExcelExportView.as_view()),
     path('employees/excel/', views.AllEmployeesExcelExportView.as_view(), name='export_all_employees_excel'),
+    path('employees/excel', views.AllEmployeesExcelExportView.as_view()),
     path('daily/<str:workday_id>/excel/', views.DailyWorkDayExcelExportView.as_view(), name='export_daily_id_excel'),
+    path('daily/<str:workday_id>/excel', views.DailyWorkDayExcelExportView.as_view()),
     path('daily/excel/', views.DailyWorkDayExcelExportView.as_view(), name='export_daily_query_excel'),
+    path('daily/excel', views.DailyWorkDayExcelExportView.as_view()),
     path('date-range/excel/', views.DateRangeExcelExportView.as_view(), name='export_date_range_excel'),
+    path('date-range/excel', views.DateRangeExcelExportView.as_view()),
     path('statistics/excel/', views.StatisticsExcelExportView.as_view(), name='export_statistics_excel'),
+    path('statistics/excel', views.StatisticsExcelExportView.as_view()),
     path('database/excel/', views.DatabaseExcelExportView.as_view(), name='export_database_excel'),
+    path('database/excel', views.DatabaseExcelExportView.as_view()),
 ]
--- config/urls.py
+++ config/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('api/employee-portal/', include('apps.employee_portal.urls')),
     path('api/', include('apps.statistics.urls')),
     path('api/exports/', include('apps.exports.urls')),
+    path('exports/', include('apps.exports.urls')),
 ]
